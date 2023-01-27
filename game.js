387ce35d6cbb220774e3178a7849eba27d90a32b
OjjuasGupta <ojjuas4453@gmail.com>
2023-01-27 20:29:12 +0530

    new

--- game.js
+++ game.js
@@ -6,4 +6,7 @@ function nextSequence(){
    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+     
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
